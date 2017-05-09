ff2b778eee2b88fe5f2384a69737328e002426d1
Levent Yıldız <levent.yildiz@ardictech.com>
2017-05-09 14:09:29 +0300

    sample alert functions added

---
+++ util.js
@@ -0,0 +1,21 @@
+UTIL = {};
+
+UTIL.ALERT = {
+	ERROR:function(){
+		alert("error");
+	},
+	SUCC:function(){
+		alert("success");
+	},
+	WARN:function(){
+		alert("warning");
+	},
+	INFO:function(){
+		alert("info");
+	}
+}
+
+//sample usage
+//UTIL.ALERT.ERROR();
+
+
